b16cec409269080ef06531941e05b6459f0d4b9a
Piyush Kumar <51694158+piyush2040@users.noreply.github.com>
2020-12-21 02:27:14 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client 
- 
-account_sid = 'AC16a07c5402ec1fc72177c2de1f17f8fa' 
-auth_token = '7471d1196f3f77f81e8da28b0180fc59' 
+#account_sid and auth_token is changed for security purposes. 
+account_sid = '###############################' 
+auth_token = '###############################' 
 client = Client(account_sid, auth_token) 
 def send_message(): 
     message = client.messages.create( 
                                 from_='+12517583965',  
-                                body='hello',      
-                                to='+916299650293' 
+                                body='This is the message that you want to send',      
+                                to='##############' 
                             ) 
  
     print(message.sid)
